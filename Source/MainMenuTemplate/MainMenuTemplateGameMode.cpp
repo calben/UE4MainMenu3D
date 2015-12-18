a272fe9151fa3aeb17471d8689f9cf9fb3c9f4b9
Calem Bendell <calem.j.bendell@gmail.com>
2015-12-18 17:11:29 -0500

    add initial project files

---
+++ Source/MainMenuTemplate/MainMenuTemplateGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MainMenuTemplate.h"
+#include "MainMenuTemplateGameMode.h"
+
+
+
+
---
+++ Source/MainMenuTemplate/MainMenuTemplateGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MainMenuTemplateGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAINMENUTEMPLATE_API AMainMenuTemplateGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
